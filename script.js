12b6da0b7176825881e927a4824dbffc64bbdad6
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-18 18:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere() {
     //Write your code here
     let radius =  document.getElementById("radius").value;
     let volume = (4/3)*Math.PI*Math.pow(radius,3);
-    volume = volume.toFixed(2);
+    volume = volume.toFixed(4);
     document.getElementById("volume").value = volume;
     return false;
 } 
